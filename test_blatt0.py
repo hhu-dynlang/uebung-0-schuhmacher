451f5df2338f5e1fc491e3609cd7484d28d7f074
lulu184 <luisa.schuhmacher@hhu.de>
2019-04-02 19:42:08 +0200

    tests palindrome and pascal

--- test_blatt0.py
+++ test_blatt0.py
@@ -1,12 +1,24 @@
-#from blatt0 import pascal
+from blatt0 import pascal_array
 #from blatt0 import flatten
 #from blatt0 import fizz_buzz
 #from blatt0 import solve_equation
 #from blatt0 import myint, mybin
+from blatt0 import is_palindrome;
 import random
 
-def test_pascal():
-    pass # TODO
+def test_is_palindrome():
+    input = "lagerregal"
+    assert is_palindrome(input) == True
+
+def test_is_not_palindrome():
+    input = "blabla"
+    assert is_palindrome(input) == False
+
+def test_pascal1():
+    assert pascal_array(5) == [1,1,1,1,2,1,1,3,3,1,1,4,6,4,1]
+
+def test_pascal_2():
+	assert pascal_array(4) == [1,1,1,1,2,1,1,3,3,1]
 
 def test_flatten():
     pass # TODO
@@ -17,14 +29,14 @@ def test_fizz_buzz():
 def test_solve_equation():
     pass # TODO
 
-def test_int_to_bin():
-    input = random.sample(range(1000), 10)
-    for x in input:
-        assert mybin(x) == bin(x)
+#def test_int_to_bin():
+#    input = random.sample(range(1000), 10)
+#    for x in input:
+#        assert mybin(x) == bin(x)
 
-def test_bin_to_int():
-    input = [(i, bin(i)) for i in random.sample(range(1000), 10)]
-    for i, bini in input:
-        assert myint(bini) == i
+#def test_bin_to_int():
+#    input = [(i, bin(i)) for i in random.sample(range(1000), 10)]
+#    for i, bini in input:
+#        assert myint(bini) == i
 
 # TODO: more tests
